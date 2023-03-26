fd9ca8f44c7eae5b550c30ae3c08a4b9eccf4ae6
Chad Bowman <chad.bowman0@gmail.com>
2023-03-26 11:02:28 -0600

    add python project files

---
+++ setup.py
@@ -0,0 +1,21 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="chadgpt",
+    version="0.1.0",
+    author="Chad Bowman",
+    author_email="chad.bowman0+github@gmail.com",
+    description="My own decoder-only transformer",
+    long_description=README,
+    long_description_type="text/markdown",
+    url="https://github.com/ChadBowman/chadgpt",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "torch ~= 2.0.0"
+    ]
+)
